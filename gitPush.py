693091c12a235e37949c6d831ef5e30720098bb6
aXi <goldenwang0425@gmail.com>
2021-08-13 00:03:53 +0800

    docs:08:12英语打卡

--- gitPush.py
+++ gitPush.py
@@ -1,10 +1,9 @@
 import os
-import sys
 
 os.system("git pull")
 os.system("git fetch --all")
 os.system("git add .")
-os.system("git commit -m docs:08:11英语打卡")
+os.system("git commit -m docs:08:12英语打卡")
 os.system("git push")
 
 
