fd2127930dbf91f6574565d35c1386bc14151ce5
aXi <goldenwang0425@gmail.com>
2021-08-13 00:11:19 +0800

    chore:版本修复

--- gitPush.py
+++ gitPush.py
@@ -4,7 +4,8 @@ os.system("git pull")
 os.system("git fetch --all")
 os.system("git add .")
 # os.system("git commit -m docs:08:12英语打卡")
-os.system("git commit -m feat:'源码 Axios 部分解析完善'")
+# os.system("git commit -m feat:'源码 Axios 部分解析完善'")
+os.system("git commit -m chore:'版本修复'")
 os.system("git push")
 
 
