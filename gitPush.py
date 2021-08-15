1e90205ba5b351e00a7494564115bc3b895e0702
aXi <goldenwang0425@gmail.com>
2021-08-15 11:09:25 +0800

    docs:08:14英语打卡

--- gitPush.py
+++ gitPush.py
@@ -3,8 +3,8 @@ import os
 os.system("git pull")
 os.system("git fetch --all")
 os.system("git add .")
-# os.system("git commit -m docs:08:12英语打卡")
-os.system("git commit -m feat:'源码 React-Router 解析部分完成'")
+os.system("git commit -m docs:08:14英语打卡")
+# os.system("git commit -m feat:'源码 React-Router 解析部分完成'")
 # os.system("git commit -m chore:'目录调整'")
 os.system("git push")
 
