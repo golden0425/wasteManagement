6a75c4733651cc9e874641f5dec6705c1ec2c9da
aXi <goldenwang0425@gmail.com>
2021-08-16 22:23:21 +0800

    docs:08:16英语打卡

--- gitPush.py
+++ gitPush.py
@@ -3,8 +3,8 @@ import os
 os.system("git pull")
 os.system("git fetch --all")
 os.system("git add .")
-# os.system("git commit -m docs:08:14英语打卡")
-os.system("git commit -m feat:'源码 Axios 排版调整'")
+os.system("git commit -m docs:08:16英语打卡")
+# os.system("git commit -m feat:'源码 Axios 排版调整'")
 # os.system("git commit -m chore:'目录调整'")
 os.system("git push")
 
