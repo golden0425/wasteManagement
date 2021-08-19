ae627b0a6551f6d2827ae99d60f8661470a2096b
aXi <goldenwang0425@gmail.com>
2021-08-19 16:18:51 +0800

    feat:长列表渲染优化

--- git.py
+++ git.py
@@ -4,7 +4,7 @@ os.system("git pull")
 os.system("git fetch --all")
 os.system("git add .")
 # os.system("git commit -m docs:08:14英语打卡")
-os.system("git commit -m feat:'Rudex 核心逻辑实现'")
+os.system("git commit -m feat:'长列表渲染优化'")
 # os.system("git commit -m chore:'目录调整'")
 os.system("git push")
 
