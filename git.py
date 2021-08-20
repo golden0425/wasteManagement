1e90a2a5536916a69bfcd1e907dc16079562a6dc
aXi <goldenwang0425@gmail.com>
2021-08-20 13:56:52 +0800

    feat:记一次with的用法

--- git.py
+++ git.py
@@ -3,8 +3,8 @@ import os
 os.system("git pull")
 os.system("git fetch --all")
 os.system("git add .")
-os.system("git commit -m docs:08:19英语打卡")
-# os.system("git commit -m feat:'长列表渲染优化'")
+# os.system("git commit -m docs:08:19英语打卡")
+os.system("git commit -m feat:'记一次with的用法'")
 # os.system("git commit -m chore:'目录调整'")
 os.system("git push")
 
