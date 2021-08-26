72b657b3b3c361d395ecf801c08443d96447a8da
aXi <goldenwang0425@gmail.com>
2021-08-26 09:03:50 +0800

    docs:08:25英语打卡

--- git.py
+++ git.py
@@ -3,8 +3,8 @@ import os
 os.system("git pull")
 os.system("git fetch --all")
 os.system("git add .")
-# os.system("git commit -m docs:08:23英语打卡")
-os.system("git commit -m feat:'Vue3 源码解析 ( 渲染函数 部分完成 )'")
+os.system("git commit -m docs:08:25英语打卡")
+# os.system("git commit -m feat:'Vue3 源码解析 ( 渲染函数 部分完成 )'")
 # os.system("git commit -m chore:'目录调整'")
 os.system("git push")
 
